7e92a0bfb46c4d8203f609da4e44a9b68ff67d6c
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-06 12:10:41 -0500

    start main script

---
+++ logistic_regression.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+class BasicLogistic(object):
+    """docstring for Logit"""
+    def __init__(self, z):
+        super().__init__()
+        self.z = z
+        
+    def sigmoid(self, z):
+        g = np.array([[0 for cols in range(2)] for rows in range(4)])
+        for row in z:
+            g = row
+
+        return g
+
+
+def main():
+    print('Still Hacking...')
+
+
+if __name__ == '__main__':
+    main()
