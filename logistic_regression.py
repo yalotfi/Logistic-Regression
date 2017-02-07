0fedbdc92f585f792a1f6f7ee3b76598e61551d5
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-06 19:51:55 -0500

    prediction method and start cost method

--- logistic_regression.py
+++ logistic_regression.py
@@ -14,15 +14,27 @@ class BasicLogistic():
         - Gradient Descent
         - Prediction function
     """
-    def __init__(self, z):
+    def __init__(self, z, X_train, y_train, learn_rate):
         super().__init__()
         self.z = z
+        self.X_train
+        self.y_train
+        self.learn_rate
         
     def sigmoid(self, z, derivative=False):
         if not derivative:
-            return 1 / (1 + np.exp(-z))
+            return 1 / (1 + np.exp(-self.z))
         else:
-            return z * (1 - z)
+            return self.z * (1 - self.z)
+
+    def hypothesis(self, X_train, theta):
+        ''' Hypothesis function (prediction method) '''
+
+    def cost_function(self, X_train, y_train, learn_rate):
+        # Number of traning examples
+        m = len(self.y_train)
+
+        preds = self.sigmoid(self.X * self.theta)
 
 
 def main():
