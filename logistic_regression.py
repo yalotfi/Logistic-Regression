babf8435f4c980acb0c2bc8556ae13c99d24c99e
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-06 20:27:06 -0500

    cost method and annotations

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,21 +6,20 @@ class BasicLogistic():
     Object to perform basic logistic regression.
 
     Currently has Methods for:
-        - Sigmoid function
+        - Logistic Sigmoid function
+        - Hypothesis function
+        - Cost function
 
     Need Methods for:
-        - Cost function
         - Regularization
         - Gradient Descent
-        - Prediction function
     """
-    def __init__(self, z, X_train, y_train, learn_rate):
+    def __init__(self, z, X_train, y_train):
         super().__init__()
         self.z = z
         self.X_train
         self.y_train
-        self.learn_rate
-        
+
     def sigmoid(self, z, derivative=False):
         if not derivative:
             return 1 / (1 + np.exp(-self.z))
@@ -28,13 +27,29 @@ class BasicLogistic():
             return self.z * (1 - self.z)
 
     def hypothesis(self, X_train, theta):
-        ''' Hypothesis function (prediction method) '''
+        '''
+        Hypothesis function (prediction method)
+        '''
 
-    def cost_function(self, X_train, y_train, learn_rate):
-        # Number of traning examples
-        m = len(self.y_train)
+        # Simple predictor
+        return self.sigmoid(self.X_train * self.theta)
 
-        preds = self.sigmoid(self.X * self.theta)
+    def cost_function(self, theta, X_train, y_train):
+        '''
+        Cost function J(theta) to be minimized
+        '''
+
+        # Initialize variables
+        m = len(self.y_train)  # Number of traning examples
+        preds = self.hypothesis(X_train, theta)
+
+        # Vectorized cost function (no iteration needed)
+        cost = -np.transpose(self.y_train) * np.log(preds)
+        cost -= (1 - np.transpose(y_train)) * np.log(1 - (preds))
+        cost *= (1 / m)
+
+        # Return unregularized costs
+        return cost
 
 
 def main():
@@ -42,7 +57,7 @@ def main():
     print('Still Hacking...')
 
     # Temp data to pass through model
-    z_test = np.array([[1,2],[3,4]])
+    z_test = np.array([[1, 2], [3, 4]])
     print(z_test)
     print(type(z_test))
 
