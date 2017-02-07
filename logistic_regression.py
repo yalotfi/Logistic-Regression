b3af146dfce76f24ab6a883c95f69f5fe8322fae
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-06 22:47:26 -0500

    testing adjustments

--- logistic_regression.py
+++ logistic_regression.py
@@ -14,42 +14,43 @@ class BasicLogistic():
         - Regularization
         - Gradient Descent
     """
-    def __init__(self, z, X_train, y_train):
+    def __init__(self, X_train, y_train, theta, reg_lamda):
         super().__init__()
-        self.z = z
-        self.X_train
-        self.y_train
+        #self.z = z
+        self.X_train = X_train
+        self.y_train = y_train
+        self.theta = theta
+        self.reg_lamda = reg_lamda
 
     def sigmoid(self, z, derivative=False):
+        ''' Logistic Sigmoid function classifies binary values '''
+        # Compute derivative or not
         if not derivative:
-            return 1 / (1 + np.exp(-self.z))
+            return 1 / (1 + np.exp(-z))
         else:
-            return self.z * (1 - self.z)
+            return z * (1 - z)
 
     def hypothesis(self, X_train, theta):
-        '''
-        Hypothesis function (prediction method)
-        '''
+        ''' Hypothesis function (prediction method) '''
+        # Simple predictor function
+        return self.sigmoid(X_train * theta)
 
-        # Simple predictor
-        return self.sigmoid(self.X_train * self.theta)
-
-    def cost_function(self, theta, X_train, y_train):
-        '''
-        Cost function J(theta) to be minimized
-        '''
-
+    def cost_function(self, X_train, y_train, theta):
+        ''' Cost function J(theta) to be minimized '''
         # Initialize variables
         m = len(self.y_train)  # Number of traning examples
         preds = self.hypothesis(X_train, theta)
 
         # Vectorized cost function (no iteration needed)
-        cost = -np.transpose(self.y_train) * np.log(preds)
+        cost = -np.transpose(y_train) * np.log(preds)
         cost -= (1 - np.transpose(y_train)) * np.log(1 - (preds))
         cost *= (1 / m)
 
         # Return unregularized costs
         return cost
+
+    def regularization(self, reg_lamda):
+        pass
 
 
 def main():
@@ -57,13 +58,22 @@ def main():
     print('Still Hacking...')
 
     # Temp data to pass through model
-    z_test = np.array([[1, 2], [3, 4]])
-    print(z_test)
-    print(type(z_test))
+    X_train = np.array([[1, 0, 1], [0, 1, 1], [1, 0, 0]])
+    y_train = np.transpose(np.array([1, 0, 1]))
+    theta = np.transpose(np.array([0, 0, 0]))
+    reg_lambda = 0.1
+    print('X Training Set: ', X_train)
+    print('y training Set: ', y_train)
+    print('Initial Parameters: ', theta)
+    print('Lamda set to: ', reg_lambda)
+    print(type(X_train))
+    print(X_train.shape)
 
     # Testing Method
-    lr = BasicLogistic(z_test)
-    print(lr.sigmoid(z_test))
+    lr = BasicLogistic(X_train, y_train, theta, reg_lambda)  # params: X_train, y_train, theta, reg_lambda
+    print(lr.sigmoid(X_train))
+    print(lr.hypothesis(X_train, theta))
+    print(lr.cost_function(X_train, y_train, theta))
 
 
 if __name__ == '__main__':
