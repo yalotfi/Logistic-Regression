44231bdfecb38224001520c4fbbdf388f990920f
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-11 15:14:47 -0500

    generalize data processing

--- logistic_regression.py
+++ logistic_regression.py
@@ -52,24 +52,54 @@ class BasicLogistic():
         pass
 
 
-def process_csv(file_path):
+def process_csv(file_path, ratio=0.5):
+    '''
+    1) Load data from a file path and split into train and test sets based on a 
+    specified ratio which set by default to a 50-50 split.
+
+    2) Adds an intercept column to the X training examples to make the math
+    cleaner... Python does not make matrix operations easy.
+    '''
     # Load Grade Data
     raw_data = np.genfromtxt(file_path, delimiter=',')
-    X = raw_data[:, 0:2]
-    y = raw_data[:, 2]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+    m_raw, n_raw = raw_data.shape  # Dims of raw_data
+
+    # Seperate labels and examples
+    X = np.zeros((m_raw, n_raw - 1))  # Exclude label column
+    for col in range(n_raw):  # Over all columns in array
+        bin_flag = np.unique(raw_data[:, col]) == [0,1]
+        if type(bin_flag) is bool:  # flag will be False if binary, list if True
+            X[:, col] = raw_data[:, col]  # Assign to X examples
+        else: # If flag matches binary test, it return a list of Trues
+            y = raw_data[:, col]  # Assign to y labels
+
+    # Split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=ratio)
+
+    # Add intercept column to training examples
+    m, n = X_train.shape  # Dimensions, m x n
+    ones = np.ones(m, dtype='int')
+    col_vecs = [X_train[:, 0], X_train[:, 1]]
+    X_train = np.vstack((ones, col_vecs)).T
+
+    # Return processed data
     return [X_train, X_test, y_train, y_test]
 
 
 def main():
-    # Load processed data
+    # STEP 1: Process data
     file_path = 'grades.txt'
     [X_train, X_test, y_train, y_test] = process_csv(file_path)
 
-    # Hyperparameters
-    m = len(X_train)  # length of the training set
-    theta = np.array([0 for rows in range(m)])  # Initialize parameters at 0
+    # STEP 2: Hyperparameters
+    m, n = X_train.shape
+    theta = np.array([0 for rows in range(n + 1)])  # Initialize parameters
     reg_lambda = 1  # Lambda value for regularization, if needed
+
+    # Training
+    lr = BasicLogistic(X_train, y_train, theta, reg_lambda)
+    print(lr.sigmoid(X_train))
+    print(lr.cost_function(X_train, y_train, theta))
 
     # Console Logs for Testing
     print('X Training Set: ', X_train.shape)
@@ -77,11 +107,6 @@ def main():
     print('Initial Parameters: ', theta.shape)
     print('Lamda set to: ', reg_lambda)
     print(type(X_train))
-
-    # Testing Class Methods
-    lr = BasicLogistic(X_train, y_train, theta, reg_lambda)
-    print(lr.sigmoid(X_train))
-    print(lr.cost_function(X_train, y_train, theta))
 
 
 if __name__ == '__main__':
