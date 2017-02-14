fe56f93c2e3c6b189e524afd1b801b509af1267c
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-14 18:58:53 -0500

    more testing?

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import logistic_regression as lr
 
 file_path = 'grades.txt'
 [X_train, X_test,
- y_train, y_test, m, n] = lr.process_csv(file_path, 0)
+ y_train, y_test, m, n] = lr.process_csv(file_path)
 
 theta = np.matrix([[0 for param in range(n + 1)]])
 reg_lambda = 1
@@ -16,6 +16,4 @@ print("X_train: ", X_train.shape, type(X_train))
 print("y_train: ", y_train.shape, type(y_train))
 print('Theta: ', theta.shape, type(theta))
 print('Matrix, X, dimensions: ', m, n)
-#print(X_train * theta.T)
-#print(lrCost.sigmoid(X_train * theta.T))
 print("Initial Cost to Minimize: ", cost)
