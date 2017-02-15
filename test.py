6afc3d2ed6e710324453f7b404a22c59b795f216
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-14 21:24:21 -0500

    update test.py

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
-import numpy as np
+# import numpy as np
 import logistic_regression as lr
 
 file_path = 'grades.txt'
 [X_train, X_test,
- y_train, y_test, m, n] = lr.process_csv(file_path)
+y_train, y_test, theta] = lr.process_csv(file_path, 0)
 
-theta = np.matrix([[0 for param in range(n + 1)]])
-reg_lambda = 1
+# theta = np.matrix([[0 for param in range(n + 1)]])
+reg_lambda = 0
 
 lrCost = lr.BasicLogistic(X_train, y_train, theta, reg_lambda)
 cost = lrCost.cost_function(X_train, y_train, theta)
+grad = lrCost.compute_grad(X_train, y_train, theta)
 
 #print(X_train)
-print("X_train: ", X_train.shape, type(X_train))
-print("y_train: ", y_train.shape, type(y_train))
+print('X_train: ', X_train.shape, type(X_train))
+print('y_train: ', y_train.shape, type(y_train))
 print('Theta: ', theta.shape, type(theta))
-print('Matrix, X, dimensions: ', m, n)
-print("Initial Cost to Minimize: ", cost)
+print('Initial Cost to Minimize: ', cost)
+print('Approx Gradient: ', grad)
