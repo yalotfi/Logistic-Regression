051f01a21793e6d3e13af55ab6c51fa32780878c
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-16 12:40:49 -0500

    edit class structure

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,5 +1,6 @@
 import numpy as np
-from sklearn.cross_validation import train_test_split
+import process_csv
+from scipy.optimize import fmin_bfgs
 
 
 class BasicLogistic():
@@ -49,6 +50,11 @@ class BasicLogistic():
         conversion produces cleaner code. Writing vectorized code is less
         error prone, easy to read, and usually faster than iterative loops.
         '''
+        # Check data types, convert to matrices
+        X_train = np.array(X_train)
+        y_train = np.matrix(y_train)
+        theta = np.matrix(theta)
+
         # Initialize helper variables
         m = len(y_train)  # Number of traning examples
         alpha = (1 / m)  # Cost Function constant
@@ -81,54 +87,6 @@ class BasicLogistic():
         pass
 
 
-def process_csv(file_path, test_size=0.3):
-    '''
-    1) Load data from a file path and split into train and test sets based on
-    specified test ratio which is 50-50 by default.
-
-    2) Adds an intercept column to the X training examples to make the math
-    cleaner... Python does not make matrix operations easy.
-    '''
-    # Load Grade Data
-    raw_data = np.genfromtxt(file_path, delimiter=',')
-    m_raw, n_raw = raw_data.shape  # Dims of raw_data
-
-    # Seperate labels and examples
-    X = np.zeros((m_raw, n_raw - 1))  # Exclude label column
-    y = np.zeros((m_raw, 1))  # init label vector
-    for col in range(n_raw):  # Over all columns in array
-        bin_flag = np.unique(raw_data[:, col]) == [0, 1]
-        if type(bin_flag) is bool:  # flag is False if binary, list if True
-            X[:, col] = raw_data[:, col]  # Assign to X examples
-        else:  # If flag matches binary test, it return a list of Trues
-            y[:, 0] = raw_data[:, col]  # Assign to y labels
-
-    # Split training set and coerce to matrices
-    [X_train, X_test,
-     y_train, y_test] = train_test_split(X, y, test_size=test_size)
-
-    # Add intercept column to training examples
-    m, n = X_train.shape  # Dimensions, m x n of X train examples
-    ones = np.ones(m, dtype='int')  # Vector of ones
-    col_vecs = []  # init a list which will store x columns
-    for col in range(n):  # n is number of columns across X_train
-        col_vecs.append(X_train[:, col])  # List cols of X_train for stacking
-    X_train = np.vstack((ones, col_vecs)).T  # Create tuple and bind X_train
-
-    # Initialize parameters, theta
-    theta = np.matrix([[0 for param in range(n + 1)]])
-
-    # Convert to numpy matrices:
-    X_train = np.matrix(X_train)
-    y_train = np.matrix(y_train)
-    X_test = np.matrix(X_test)
-    y_test = np.matrix(y_test)
-
-    # Return processed data
-    return [X_train, X_test,
-            y_train, y_test, theta]  # Return train, test, and parameter sets
-
-
 def main():
     # STEP 1: Process data
     file_path = 'grades.txt'
@@ -137,7 +95,7 @@ def main():
 
     # STEP 2: Hyperparameters
     # theta = np.matrix([[0 for param in range(n + 1)]])  # Initialize params
-    reg_lambda = 1  # Lambda value for regularization, if needed
+    reg_lambda = 0  # Lambda value for regularization, if needed
 
     # Console Logs for Testing
     print('X Training Set: ', X_train.shape)
@@ -145,12 +103,20 @@ def main():
     print('y transpose: ', np.transpose(y_train).shape)
     print('Params: {0} Size: {1}'.format(theta, theta.shape))
     print('Lamda set to: ', reg_lambda)
-    print(type(X_train))
-    print(X_train[0:10, :])
 
     # Step 3: Training
     lr = BasicLogistic(X_train, y_train, theta, reg_lambda)
-    print(lr.sigmoid((X_train * theta)[0:10, :]))
+
+    # Optimization
+    initial_theta = np.array(theta)
+    opt_theta = fmin_bfgs(lr.cost_function(X_train, y_train, theta), 
+        initial_theta,
+        fprime=lr.compute_grad(X_train, y_train, theta),
+        maxiter=400
+    )
+
+    print(opt_theta)
+
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-# import numpy as np
 import logistic_regression as lr
+import process_csv
+
 
 file_path = 'grades.txt'
 [X_train, X_test,
-y_train, y_test, theta] = lr.process_csv(file_path, 0)
+y_train, y_test, theta] = process_csv(file_path, 0)
 
 # theta = np.matrix([[0 for param in range(n + 1)]])
 reg_lambda = 0
@@ -11,6 +12,7 @@ reg_lambda = 0
 lrCost = lr.BasicLogistic(X_train, y_train, theta, reg_lambda)
 cost = lrCost.cost_function(X_train, y_train, theta)
 grad = lrCost.compute_grad(X_train, y_train, theta)
+opt_theta = lrCost.optimize(X_train, y_train, theta)
 
 #print(X_train)
 print('X_train: ', X_train.shape, type(X_train))
@@ -18,3 +20,4 @@ print('y_train: ', y_train.shape, type(y_train))
 print('Theta: ', theta.shape, type(theta))
 print('Initial Cost to Minimize: ', cost)
 print('Approx Gradient: ', grad)
+print('Optimal Parameters: ', opt_theta)
