d461f0f27a61d31bef8920e4fe8d53efe87e0dbc
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-22 12:26:25 -0500

    test bfgs algo seperately

--- optimize.py
+++ optimize.py
@@ -2,10 +2,12 @@ import numpy as np
 from scipy.optimize import fmin_bfgs
 
 
+# Rosenbrock Function
 def rosen(x):
     return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
 
 
+# Broyden-Fletcher-Goldfarb-Shanno Algorithm Gradient (fmin_bfgs)
 def rosen_der(x):
      xm = x[1:-1]
      xm_m1 = x[:-2]
@@ -16,8 +18,7 @@ def rosen_der(x):
      der[-1] = 200*(x[-1]-x[-2]**2)
      return der
 
+
 x0 = [1.3, 0.7, 0.8, 1.9, 1.2]
-
 xopt = fmin_bfgs(rosen, x0, fprime=rosen_der)
-
 print(xopt)
