9baeb0f67db44be322f416e6a7d3628144459c15
Yaseen Lotfi <yalotfi@outlook.com>
2017-03-21 14:11:31 -0400

    method for computing gradient steps of loss function and remove gradient descent method

--- BasicLogistic.py
+++ BasicLogistic.py
@@ -1,5 +1,5 @@
 import numpy as np
-import sigmoid as sm
+from sigmoid import _sigmoid
 import process_csv as data
 
 
@@ -24,6 +24,7 @@ class BasicLogistic():
         self.y_train = y_train
         self.init_theta = init_theta
         self.reg_lamda = reg_lamda
+        self.m = len(self.y_train)
 
     def hypothesis(self, theta):
         '''
@@ -34,7 +35,7 @@ class BasicLogistic():
         This function is used in calculating the error and partial derivative
         for fitting the best decision boundary on the given data set.
         '''
-        return sm.sigmoid(self.X_train.dot(theta.T))
+        return _sigmoid(self.X_train.dot(theta.T))
 
     def cost_function(self, theta):
         '''
@@ -44,50 +45,35 @@ class BasicLogistic():
         the label is 0, then the positive case is computed. The magic of this
         cost function is that it represents either case in one equation.
         '''
-        # Initialize helper variables
-        m = len(self.y_train)  # Number of traning labels
-        const = (1 / m)  # Cost Function constant
+        const = (1 / self.m)  # Cost Function constant
 
         # Cost Function
         pred = self.hypothesis(theta)
-        neg_case = -self.y_train.T.dot(np.log(pred))
-        pos_case = (1 - self.y_train.T).dot(np.log(1 - pred))
+        loss00 = -self.y_train.T.dot(np.log(pred))
+        loss01 = (1 - self.y_train.T).dot(np.log(1 - pred))
+        reg_cost = self.reg_lamda / (2 * self.m) * theta.T.dot(theta)
+        cost = const * (loss00 - loss01) + reg_cost
 
         # Return Cost
-        cost = const * (neg_case - pos_case)  # (1x1)-(1x1)=(1x1)
+        return float(cost)
 
-        # Compute gradient Step
+    def compute_gradient(self, theta):
+        const = (1 / self.m)
+
+        # Gradient Step
         pred = self.hypothesis(theta)
-        grad = 1 / m * self.X_train.T.dot(pred - self.y_train).T
+        loss = pred - self.y_train
+        step = const * self.X_train.T.dot(loss)
 
-        return [cost[0][0], grad[0]]
+        # Add regularization term
+        reg_grad = self.reg_lambda / self.m * theta[1:, :]
+        grad00 = step[0, :]
+        grad01 = step[1:, :] + reg_grad
+        grads = np.append(grad00, grad01)
 
-    def gradient_descent(self, alpha, max_iters):
-        '''
-        Compute the gradient, or partial derivative, of the calculated cost.
-        This is used to optimize the learning algorithm's parameters that fit
-        some prediction or hypothesis function to the data. Minimizing the
-        cost by an optimization function is basically searching for the global
-        minimum of the function.
-        '''
-        m = float(len(self.X_train))
-        J_history = [0] * max_iters
-        theta = self.init_theta
+        return grads
 
-        for i in range(max_iters):
-            pred = self.hypothesis(theta)
-            gradient = self.X_train.T.dot(pred - self.y_train)
-            theta = theta - alpha / m * gradient.T
-
-            cost = self.cost_function(theta)
-            J_history[i] = cost
-
-        return theta, np.array(J_history)
-
-    def map_feature(X):
-        pass
-
-    def regularization(self):
+    def map_feature(self, X):
         pass
 
 
@@ -98,7 +84,7 @@ def main():
      y_train, y_test, theta] = data.process_csv(file_path)
 
     # STEP 2: Hyperparameters
-    alpha = 0.000001
+    # alpha = 0.000001
     reg_lambda = 0  # Lambda value for regularization, if needed
 
     # Console logs to confirm data structure
